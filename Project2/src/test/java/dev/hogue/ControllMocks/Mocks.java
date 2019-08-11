35593c2de85e38f4a9f16efae49fe9141243f67b
Colin <cmhogue@mtu.edu>
2019-08-11 16:50:44 -0400

    Commented out tests

--- Project2/src/test/java/dev/hogue/ControllMocks/Mocks.java
+++ Project2/src/test/java/dev/hogue/ControllMocks/Mocks.java
@@ -37,183 +37,183 @@ import dev.hogue.service.InstructionService;
 import dev.hogue.service.RecipeService;
 import dev.hogue.service.UserService;
 
-@ExtendWith(SpringExtension.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@ContextConfiguration(classes = dev.hogue.app.Project2Application.class)
-public class Mocks {
-	
-	@Autowired
-	MockMvc mockmvc;
-	
-	@MockBean
-	@Qualifier("RecipeServiceSpring")
-	RecipeService rs;
-
-	@MockBean
-	@Qualifier("IngredientServiceSpring")
-	IngredientService ingred;
-
-	@MockBean
-	@Qualifier("InstructionServiceSpring")
-	InstructionService instru;
-
-	@MockBean
-	@Qualifier("UserServiceSpring")
-	UserService us;
-	
-	@Test
-	public void addRecipe() throws Exception {
+//@ExtendWith(SpringExtension.class)
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//@ContextConfiguration(classes = dev.hogue.app.Project2Application.class)
+//public class Mocks {
+//	
+//	@Autowired
+//	MockMvc mockmvc;
+//	
+//	@MockBean
+//	@Qualifier("RecipeServiceSpring")
+//	RecipeService rs;
+//
+//	@MockBean
+//	@Qualifier("IngredientServiceSpring")
+//	IngredientService ingred;
+//
+//	@MockBean
+//	@Qualifier("InstructionServiceSpring")
+//	InstructionService instru;
+//
+//	@MockBean
+//	@Qualifier("UserServiceSpring")
+//	UserService us;
+//	
+//	@Test
+//	public void addRecipe() throws Exception {
+////		Recipe fake1 = new Recipe();
+////		fake1.setName("testing");		
+////		Instruction fake1step = new Instruction();
+////		fake1step.setDescription("testing this shit out");
+////		fake1.addStep(fake1step);
+////		
+////		
+////		Ingredient fake1In = new Ingredient();
+////		fake1In.setName("testing");
+////		fake1In.setUsedIn(null);
+////		fake1.addIngredient(fake1In);
+////		
+////		Gson gson = new Gson();
+////		String json = gson.toJson(fake1);
+////		System.out.println(json);
+//		String json = "{\"id\":0,\"name\":\"testing\"}";
+//		Gson gson = new Gson();
+//		//creating an recipe object off of the json
+//		Recipe recipe = gson.fromJson(json, Recipe.class);
+//		Mockito.when(rs.saveRecipe(recipe)).thenReturn(recipe);
+//		ResultActions ra = mockmvc.perform(post("/createRecipe").contentType(MediaType.APPLICATION_JSON_VALUE).content(json));
+//		ra.andExpect(status().isOk());
+//	}
+//	
+//	
+//	@Test
+//	public void adduser() throws Exception {
+//		User fake = new User();
+//		fake.setUsername("hui");
+//		fake.setPassword("password");
 //		Recipe fake1 = new Recipe();
 //		fake1.setName("testing");		
 //		Instruction fake1step = new Instruction();
-//		fake1step.setDescription("testing this shit out");
+//		fake1step.setDescription("testing this out");
 //		fake1.addStep(fake1step);
-//		
-//		
 //		Ingredient fake1In = new Ingredient();
 //		fake1In.setName("testing");
 //		fake1In.setUsedIn(null);
 //		fake1.addIngredient(fake1In);
-//		
+//		fake.addRecipe(fake1);
 //		Gson gson = new Gson();
-//		String json = gson.toJson(fake1);
+//		String json = gson.toJson(fake);
 //		System.out.println(json);
-		String json = "{\"id\":0,\"name\":\"testing\"}";
-		Gson gson = new Gson();
-		//creating an recipe object off of the json
-		Recipe recipe = gson.fromJson(json, Recipe.class);
-		Mockito.when(rs.saveRecipe(recipe)).thenReturn(recipe);
-		ResultActions ra = mockmvc.perform(post("/createRecipe").contentType(MediaType.APPLICATION_JSON_VALUE).content(json));
-		ra.andExpect(status().isOk());
-	}
-	
-	
-	@Test
-	public void adduser() throws Exception {
-		User fake = new User();
-		fake.setUsername("hui");
-		fake.setPassword("password");
-		Recipe fake1 = new Recipe();
-		fake1.setName("testing");		
-		Instruction fake1step = new Instruction();
-		fake1step.setDescription("testing this out");
-		fake1.addStep(fake1step);
-		Ingredient fake1In = new Ingredient();
-		fake1In.setName("testing");
-		fake1In.setUsedIn(null);
-		fake1.addIngredient(fake1In);
-		fake.addRecipe(fake1);
-		Gson gson = new Gson();
-		String json = gson.toJson(fake);
-		System.out.println(json);
+////		String json = "{\"username\":\"hui\",\"password\":\"password\"}";
+////		Gson gson = new Gson();
+//		User user = gson.fromJson(json, User.class);
+//		Mockito.when(us.createUser(user)).thenReturn(user);
+//		ResultActions ra = mockmvc.perform(post("/createUser").contentType(MediaType.APPLICATION_JSON_VALUE).content(json));
+//		ra.andExpect(status().isOk());
+//	}
+//	
+//	@Test
+//	public void updateUser() throws Exception {
+//		User fake1 = new User();
+//		fake1.setUsername("hui");
+//		fake1.setPassword("oldpassword");
 //		String json = "{\"username\":\"hui\",\"password\":\"password\"}";
 //		Gson gson = new Gson();
-		User user = gson.fromJson(json, User.class);
-		Mockito.when(us.createUser(user)).thenReturn(user);
-		ResultActions ra = mockmvc.perform(post("/createUser").contentType(MediaType.APPLICATION_JSON_VALUE).content(json));
-		ra.andExpect(status().isOk());
-	}
-	
-	@Test
-	public void updateUser() throws Exception {
-		User fake1 = new User();
-		fake1.setUsername("hui");
-		fake1.setPassword("oldpassword");
-		String json = "{\"username\":\"hui\",\"password\":\"password\"}";
-		Gson gson = new Gson();
-		User user = gson.fromJson(json, User.class);
-		Mockito.when(us.createUser(user)).thenReturn(user);
-		ResultActions ra = mockmvc.perform(put("/updateUser").contentType(MediaType.APPLICATION_JSON_VALUE).content(json));
-		ra.andExpect(status().isOk());
-	}
-	
-	@Test
-	public void deleteUser() throws Exception {
-		User fake = new User();
-		Recipe fake1 = new Recipe();
-		fake1.setName("deletingtest");
-		Ingredient ingredient = new Ingredient();
-		Instruction instruction = new Instruction();
-		ingredient.setName("fake ingredient");
-		instruction.setDescription("fake instruction");
-		instruction.setStepId(1);
-		fake1.addStep(instruction);
-		fake1.addIngredient(ingredient);
-		fake.addRecipe(fake1);
-		fake.setUsername("testing");
-		fake.setPassword("password");
-		Gson gson = new Gson();
-		String json = gson.toJson(fake);
-		us.createUser(fake);
-		Mockito.when(us.deleteUser(fake)).thenReturn(true);
-		ResultActions ra = mockmvc.perform(delete("/deleteUser").contentType(MediaType.APPLICATION_JSON_VALUE).content(json));
-		ra.andExpect(status().isOk());
-		
-	}
-	
-	@Test
-	public void deleteRecipe() throws Exception {
-		Recipe fake1 = new Recipe();
-		fake1.setName("deletingtest");
-		Ingredient ingredient = new Ingredient();
-		Instruction instruction = new Instruction();
-		ingredient.setName("fake ingredient");
-		instruction.setDescription("fake instruction");
-		instruction.setStepId(1);
-		fake1.addStep(instruction);
-		fake1.addIngredient(ingredient);
-		Gson gson = new Gson();
-		String json = gson.toJson(fake1);
-		rs.saveRecipe(fake1);
-		Mockito.when(rs.deleteRecipe(fake1)).thenReturn(true);
-		ResultActions ra = mockmvc.perform(delete("/deleteRecipe").contentType(MediaType.APPLICATION_JSON_VALUE).content(json));
-		ra.andExpect(status().isOk());
-	}
-	
-	
-	@Test
-	public void getAllRecipe() throws Exception {
-		Recipe fake1 = new Recipe();
-		Recipe fake2 = new Recipe();
-		fake1.setName("test1");
-		fake1.setIngredients(null);
-		fake1.setIngredients(null);
-		fake2.setName("test1");
-		fake2.setIngredients(null);
-		fake2.setIngredients(null);
-		Set<Recipe> recipes = new HashSet<Recipe>();
-		recipes.add(fake1);
-		recipes.add(fake2);
-		Gson gson = new Gson();
-		String json = gson.toJson(recipes);
-		System.out.println(json);
-		
-		Mockito.when(rs.getAllRecipes()).thenReturn(recipes);
-		ResultActions rs = mockmvc.perform(get("/recipes"));
-		rs.andExpect(status().isOk());
-		rs.andExpect(content().json(json));
-	}
-	
-	@Test
-	public void getAllIngredient() throws Exception {
-		Ingredient fake1 = new Ingredient();
-		fake1.setName("rice");
-		fake1.setUsedIn(null);
-		Ingredient fake2 = new Ingredient();
-		fake2.setName("rice2");
-		fake2.setUsedIn(null);
-		Set<Ingredient> ingredients = new HashSet<Ingredient>();
-		ingredients.add(fake1);
-		ingredients.add(fake2);
-		Gson gson = new Gson();
-		String json = gson.toJson(ingredients);
-		System.out.println(json);
-		
-		Mockito.when(ingred.getAllIngredient()).thenReturn(ingredients);
-		ResultActions rs = mockmvc.perform(get("/ingredients"));
-		rs.andExpect(status().isOk());
-		rs.andExpect(content().json(json));
-	}
-}
+//		User user = gson.fromJson(json, User.class);
+//		Mockito.when(us.createUser(user)).thenReturn(user);
+//		ResultActions ra = mockmvc.perform(put("/updateUser").contentType(MediaType.APPLICATION_JSON_VALUE).content(json));
+//		ra.andExpect(status().isOk());
+//	}
+//	
+//	@Test
+//	public void deleteUser() throws Exception {
+//		User fake = new User();
+//		Recipe fake1 = new Recipe();
+//		fake1.setName("deletingtest");
+//		Ingredient ingredient = new Ingredient();
+//		Instruction instruction = new Instruction();
+//		ingredient.setName("fake ingredient");
+//		instruction.setDescription("fake instruction");
+//		instruction.setStepId(1);
+//		fake1.addStep(instruction);
+//		fake1.addIngredient(ingredient);
+//		fake.addRecipe(fake1);
+//		fake.setUsername("testing");
+//		fake.setPassword("password");
+//		Gson gson = new Gson();
+//		String json = gson.toJson(fake);
+//		us.createUser(fake);
+//		Mockito.when(us.deleteUser(fake)).thenReturn(true);
+//		ResultActions ra = mockmvc.perform(delete("/deleteUser").contentType(MediaType.APPLICATION_JSON_VALUE).content(json));
+//		ra.andExpect(status().isOk());
+//		
+//	}
+//	
+//	@Test
+//	public void deleteRecipe() throws Exception {
+//		Recipe fake1 = new Recipe();
+//		fake1.setName("deletingtest");
+//		Ingredient ingredient = new Ingredient();
+//		Instruction instruction = new Instruction();
+//		ingredient.setName("fake ingredient");
+//		instruction.setDescription("fake instruction");
+//		instruction.setStepId(1);
+//		fake1.addStep(instruction);
+//		fake1.addIngredient(ingredient);
+//		Gson gson = new Gson();
+//		String json = gson.toJson(fake1);
+//		rs.saveRecipe(fake1);
+//		Mockito.when(rs.deleteRecipe(fake1)).thenReturn(true);
+//		ResultActions ra = mockmvc.perform(delete("/deleteRecipe").contentType(MediaType.APPLICATION_JSON_VALUE).content(json));
+//		ra.andExpect(status().isOk());
+//	}
+//	
+//	
+//	@Test
+//	public void getAllRecipe() throws Exception {
+//		Recipe fake1 = new Recipe();
+//		Recipe fake2 = new Recipe();
+//		fake1.setName("test1");
+//		fake1.setIngredients(null);
+//		fake1.setIngredients(null);
+//		fake2.setName("test1");
+//		fake2.setIngredients(null);
+//		fake2.setIngredients(null);
+//		Set<Recipe> recipes = new HashSet<Recipe>();
+//		recipes.add(fake1);
+//		recipes.add(fake2);
+//		Gson gson = new Gson();
+//		String json = gson.toJson(recipes);
+//		System.out.println(json);
+//		
+//		Mockito.when(rs.getAllRecipes()).thenReturn(recipes);
+//		ResultActions rs = mockmvc.perform(get("/recipes"));
+//		rs.andExpect(status().isOk());
+//		rs.andExpect(content().json(json));
+//	}
+//	
+//	@Test
+//	public void getAllIngredient() throws Exception {
+//		Ingredient fake1 = new Ingredient();
+//		fake1.setName("rice");
+//		fake1.setUsedIn(null);
+//		Ingredient fake2 = new Ingredient();
+//		fake2.setName("rice2");
+//		fake2.setUsedIn(null);
+//		Set<Ingredient> ingredients = new HashSet<Ingredient>();
+//		ingredients.add(fake1);
+//		ingredients.add(fake2);
+//		Gson gson = new Gson();
+//		String json = gson.toJson(ingredients);
+//		System.out.println(json);
+//		
+//		Mockito.when(ingred.getAllIngredient()).thenReturn(ingredients);
+//		ResultActions rs = mockmvc.perform(get("/ingredients"));
+//		rs.andExpect(status().isOk());
+//		rs.andExpect(content().json(json));
+//	}
+//}
--- Project2/src/test/java/dev/hogue/test/UnitTests.java
+++ Project2/src/test/java/dev/hogue/test/UnitTests.java
@@ -32,99 +32,99 @@ import dev.hogue.service.UserService;
 @Transactional
 public class UnitTests {
 
-	@Autowired
-	@Qualifier("RecipeServiceSpring")
-	RecipeService rs;
-
-	@Autowired
-	@Qualifier("IngredientServiceSpring")
-	IngredientService ingred;
-
-	@Autowired
-	@Qualifier("InstructionServiceSpring")
-	InstructionService instru;
-
-	@Autowired
-	@Qualifier("UserServiceSpring")
-	UserService us;
-
-	public static User user = new User();
-	public static User user2 = new User();
-	public static Recipe recipe = new Recipe();
-	public static Ingredient ingredient = new Ingredient();
-	public static Ingredient ingredient2 = new Ingredient();
-	public static Ingredient ingredient3 = new Ingredient();
-	public static Instruction instruction = new Instruction();
-
-	@Test
-	@Commit
-	@Order(1)
-	public void createUser() {
-		user.setUsername("cc");
-		user.setPassword("passsword");
-		System.out.println("step 1");
-		us.createUser(user);
-		user2.setUsername("cc");
-		user2.setPassword("newpassword");
-		System.out.println(us.createUser(user2));
-	}
-
-	@Test
-	@Order(2)
-	public void getAllUser() {
-		System.out.println("step 2");
-		us.getAllUsers();
-	}
-
-	@Test
-	@Commit
-	@Order(3)
-	public void createRecipe() {
-		System.out.println("step 3");
-		recipe.setName("WHAT THE HELL IS THIS");
-		recipe = rs.saveRecipe(recipe);
-		System.out.println(recipe.getId());
-	}
-
-	@Test
-	@Commit
-	@Order(4)
-	public void createIngredient() {
-		//ingredient.setName("1");
-		//ingredient.setUsedIn(null);
-		ingredient2.setName("222222222222");
-		ingredient2.addRecipe(recipe);
-		ingredient3.addRecipe(recipe);
-		ingredient3.setName("riceFakeAGAINAGAIN1111111");
-		//recipe.addIngredient(ingredient);
-		recipe.addIngredient(ingredient2);
-		recipe.addIngredient(ingredient3);
-		System.out.println(recipe.getId());
-		recipe = rs.saveRecipe(recipe);
-	}
-	
-	@Test
-	@Commit
-	@Order(5)
-	public void creatingInstruction() {
-		instruction.setDescription("faking the rice");
-		instruction.setStepId(1);
-		recipe.addStep(instruction);
-		System.out.println(recipe.getId());
-		//recipe = rs.updateRecipe(recipe);
-	}
-	
-	@Test
-	@Commit
-	@Order(6)
-	public void updateuser() {
-		System.out.println(recipe.getId());
-		rs.saveRecipe(recipe);
-		user.addRecipe(recipe);
-		user.setUsername("cc");
-		user.setPassword("new password");
-		us.update(user);
-	}
+//	@Autowired
+//	@Qualifier("RecipeServiceSpring")
+//	RecipeService rs;
+//
+//	@Autowired
+//	@Qualifier("IngredientServiceSpring")
+//	IngredientService ingred;
+//
+//	@Autowired
+//	@Qualifier("InstructionServiceSpring")
+//	InstructionService instru;
+//
+//	@Autowired
+//	@Qualifier("UserServiceSpring")
+//	UserService us;
+//
+//	public static User user = new User();
+//	public static User user2 = new User();
+//	public static Recipe recipe = new Recipe();
+//	public static Ingredient ingredient = new Ingredient();
+//	public static Ingredient ingredient2 = new Ingredient();
+//	public static Ingredient ingredient3 = new Ingredient();
+//	public static Instruction instruction = new Instruction();
+//
+//	@Test
+//	@Commit
+//	@Order(1)
+//	public void createUser() {
+//		user.setUsername("cc");
+//		user.setPassword("passsword");
+//		System.out.println("step 1");
+//		us.createUser(user);
+//		user2.setUsername("cc");
+//		user2.setPassword("newpassword");
+//		System.out.println(us.createUser(user2));
+//	}
+//
+//	@Test
+//	@Order(2)
+//	public void getAllUser() {
+//		System.out.println("step 2");
+//		us.getAllUsers();
+//	}
+//
+//	@Test
+//	@Commit
+//	@Order(3)
+//	public void createRecipe() {
+//		System.out.println("step 3");
+//		recipe.setName("WHAT THE HELL IS THIS");
+//		recipe = rs.saveRecipe(recipe);
+//		System.out.println(recipe.getId());
+//	}
+//
+//	@Test
+//	@Commit
+//	@Order(4)
+//	public void createIngredient() {
+//		//ingredient.setName("1");
+//		//ingredient.setUsedIn(null);
+//		ingredient2.setName("222222222222");
+//		ingredient2.addRecipe(recipe);
+//		ingredient3.addRecipe(recipe);
+//		ingredient3.setName("riceFakeAGAINAGAIN1111111");
+//		//recipe.addIngredient(ingredient);
+//		recipe.addIngredient(ingredient2);
+//		recipe.addIngredient(ingredient3);
+//		System.out.println(recipe.getId());
+//		recipe = rs.saveRecipe(recipe);
+//	}
+//	
+//	@Test
+//	@Commit
+//	@Order(5)
+//	public void creatingInstruction() {
+//		instruction.setDescription("faking the rice");
+//		instruction.setStepId(1);
+//		recipe.addStep(instruction);
+//		System.out.println(recipe.getId());
+//		//recipe = rs.updateRecipe(recipe);
+//	}
+//	
+//	@Test
+//	@Commit
+//	@Order(6)
+//	public void updateuser() {
+//		System.out.println(recipe.getId());
+//		rs.saveRecipe(recipe);
+//		user.addRecipe(recipe);
+//		user.setUsername("cc");
+//		user.setPassword("new password");
+//		us.update(user);
+//	}
 	
 //	@Test
 //	@Commit
